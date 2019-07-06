593e7dda162a2da7f08472976a5428d6382a3565
Andrew Jones <andrew@bidops.com>
2019-07-06 14:12:24 -0400

    url and tracking code

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'andrewcjones.dev',
   pathPrefix: '/',
   title: 'React Portfolio by Andrew Jones',
   subtitle: 'Startup Cofounder & CTO. Javascript + React. Former neuroscience researcher at Emory University.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-124019593-2',
   menu: [
     {
       label: 'Articles',
@@ -27,7 +27,6 @@ module.exports = {
     name: 'Andrew Jones',
     photo: '/photo.png',
     bio: 'Startup Cofounder & CTO. Javascript + React. Former neuroscience researcher at Emory University.',
-    
     // TODO: remove twitter, rss, telegram, vkontakte from pages, tests, and fixtures
     contacts: {
       email: 'andrewjones232@gmail.com',
